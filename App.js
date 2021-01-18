15b5bf84821cc87c5338479da2b8e68ee9d85818
dlwong <starletfordorris@gmail.com>
2021-01-18 11:27:14 -0800

    Add questions

--- App.js
+++ App.js
@@ -13,12 +13,42 @@ function Home({ navigation }) {
     );
 }
 
-function QuestionScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Question Screen</Text>
-    </View>
-  );
+class QuestionScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        predict: 0,
+        questions: [
+            {
+                id: 1,
+                title: 'Who are you?'
+            },
+            {
+                id: 2,
+                title: 'Who are you2?'
+            },
+            {
+                id: 3,
+                title: 'Who are you3?'
+            }
+        ]
+    }
+}
+
+  componentDidMount() {
+    const rand = Math.floor(Math.random() * (3 - 1)) + 1;
+    this.setState({predict: rand})
+  }
+
+  render() {
+    const num = this.state.predict;
+
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>{ this.state.questions[num].title }</Text>
+      </View>
+    );
+  }
 }
 
 const Stack = createStackNavigator();
