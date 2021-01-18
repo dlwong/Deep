3f2751b60ffcbfd44cf230729d839a9fd64bd195
dlwong <starletfordorris@gmail.com>
2021-01-18 15:16:19 -0800

    Fix glitch and change array

--- src/components/QuestionScreen.js
+++ src/components/QuestionScreen.js
@@ -1,23 +1,20 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 import questionsList from '../questionsList.js';
 
 export default function QuestionScreen() {
   const max = questionsList.length;
   const min = 0;
+
   function rand() {
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
   const [count, setCount] = useState( rand() );
 
-  // useEffect(() => {
-  //   runAgain()
-  // });
-
   return (
     <View style={styles.container}>
-      <Text>{ count }</Text>
+      <Text>{ questionsList[count] }</Text>
         <View style={styles.buttonContainer}>
           <Button
           title="Another Question"
--- src/questionsList.js
+++ src/questionsList.js
@@ -1,16 +1,8 @@
 const questionsList = [
-  {
-    title: 'What would you miss the most if we stopped being friends?'
-  },
-  {
-    title: 'What annoys you about me, but you are over it?'
-  },
-  {
-    title: 'If my mom asked me about you, what would I say?'
-  },
-  {
-    title: 'Could we stay friends if I shared that I was interested in your ex?'
-  }
+  'What would you miss the most if we stopped being friends?',
+  'What annoys you about me, but you are over it?',
+  'If my mom asked me about you, what would I say?',
+  'Could we stay friends if I shared that I was interested in your ex?',
 ];
 
 export default questionsList;
