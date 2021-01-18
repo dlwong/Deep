43da42a472459f816eb246e47b8e099b12c956be
dlwong <starletfordorris@gmail.com>
2021-01-18 11:04:34 -0800

    Route Home to Details

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Image, TouchableHighlight } from 'react-native';
+import { StyleSheet, View, Image, TouchableHighlight, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-class Home extends Component {
-  _onPressButton() {
-    console.log("Pushed button Start the Game!")
-  }
-
-  render() {
+function Home({ navigation }) {
     return (
       <View style={styles.container}>
-        <TouchableHighlight onPress = {this._onPressButton} >
-        <Image style={styles.imageContainer} source={require('./icon.png')}/>
+        <TouchableHighlight onPress = {() => navigation.navigate('Details')} >
+          <Image style={styles.imageContainer} source={require('./icon.png')}/>
         </TouchableHighlight>
       </View>
     );
-  }
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
 }
 
 const Stack = createStackNavigator();
@@ -24,8 +26,9 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
